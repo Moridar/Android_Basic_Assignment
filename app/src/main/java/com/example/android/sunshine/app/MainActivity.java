7979bf588a582f2a82698ada46ee121c7917eb4b
Bobbie <bobbieapitzsch@gmail.com>
2016-02-26 23:17:01 +0100

    g

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -8,6 +8,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -49,6 +53,7 @@ public class MainActivity extends ActionBarActivity {
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
+        ArrayAdapter<String> mForecastAdaptar;
 
         public PlaceholderFragment() {
         }
@@ -57,6 +62,19 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            List<String> weekForecast = new ArrayList<String>();
+            weekForecast.add("Today - Sunny - 88 / 63");
+            weekForecast.add("Tomorrow - Foggy - 70 / 46");
+            weekForecast.add("Weds - CLody - 72 / 63");
+            weekForecast.add("Thurs - Rainy - 64 / 51");
+            weekForecast.add("Fri - Foggy - 70 / 46");
+            weekForecast.add("Sat - Sunny - 76 / 68");
+            weekForecast.add("Sun - Sunny - 50 / 60");
+            weekForecast.add("Mon - Foggy - 99 / 50");
+
+            mForecastAdaptar = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
+
             return rootView;
         }
     }
